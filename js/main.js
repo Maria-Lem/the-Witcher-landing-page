35033b88eb7777e17b4dba83d706d375df9296d5
Maria <maria131621@yandex.ru>
2021-02-23 22:47:29 +0300

    feat: add responsive layout

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ const mySwiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
   loop: true,
   speed: 400,
-  slidesPerView: 2,
-  // loopedSlides: 2,
-
+  slidesPerView: 1,
+  breakpoints: {
+    541: {
+      slidesPerView: 2
+    }
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.arrow',
   },
+})
+
+const menuButton = document.querySelector('.menu-button'),
+  menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('header-active');
+  menuButton.classList.toggle('menu-button-active');
 })
